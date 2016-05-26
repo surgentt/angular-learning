df1ede1e6cd89dbfacbacf30dfa99b1f5b93681f
Thomas Surgent <surgentt@gmail.com>
2016-05-26 11:31:12 -0400

    Components now works

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -3,22 +3,37 @@ var phonecatApp = angular.module('phonecatApp', []);
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
 phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
-  $scope.phones = [
-    {
-      name: 'Nexus S',
-      snippet: 'Fast just got faster with Nexus S.'
-    }, {
-      name: 'Motorola XOOM™ with Wi-Fi',
-      snippet: 'The Next, Next Generation tablet.'
-    }, {
-      name: 'MOTOROLA XOOM™',
-      snippet: 'The Next, Next Generation tablet.'
-    }
-  ];
-
   // Creation of a second scope
   $scope.name = 'world';
 
   // Creation of a third scope
   $scope.list = [0, 1, 2, 3, 4, 5, 6, 7]
 });
+
+// Register `phoneList` component, along with its associated controller and template
+angular.
+  module('phonecatApp').
+  component('phoneList', {
+    template:
+    '<ul>' +
+    '<li ng-repeat="phone in $ctrl.phones">' + // the $crtl references to an instance of this controller, not root scope.
+    '<span>{{phone.name}}</span>' +
+    '<p>{{phone.snippet}}</p>' +
+    '</li>' +
+    '</ul>' +
+    '<p>Total number of phones: {{$ctrl.phones.length}}</p>',
+    controller: function PhoneListController() {
+      this.phones = [
+        {
+          name: 'Nexus S',
+          snippet: 'Fast just got faster with Nexus S.'
+        }, {
+          name: 'Motorola XOOM™ with Wi-Fi',
+          snippet: 'The Next, Next Generation tablet.'
+        }, {
+          name: 'MOTOROLA XOOM™',
+          snippet: 'The Next, Next Generation tablet.'
+        }
+      ];
+    }
+  });
